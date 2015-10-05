a1d5b189ae231d7083dfb88da3523284bb3594a3
Jorge Epuñan <juanbrujo@gmail.com>
2015-10-05 15:30:03 -0300

    Several improvements: perf, re-order
    - added jit-grunt to improve speed
    - new file structure: js is now located on theme-root and not in a
    sets/js/, same for images
    - removed grunt-concat (not being used)

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
 	
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/dist/assets/js/libs/jquery-1.11.2.min.js"><\/script>')</script>
-    <!--[if (gte IE 6)&(lte IE 8)]>
-    <script src="<?php echo get_template_directory_uri(); ?>/dist/assets/js/libs/selectivizr.min.js"></script>
-    <![endif]-->
-    <script src="<?php echo get_template_directory_uri(); ?>/dist/assets/js/functions.min.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/dist/assets/js/functions-index.min.js"></script>
+	<script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/libs/jquery-1.11.2.min.js"><\/script>')</script>
+	<!--[if (gte IE 6)&(lte IE 8)]>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/libs/selectivizr.min.js"></script>
+	<![endif]-->
+	<script src="<?php echo get_template_directory_uri(); ?>/js/functions.min.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/functions-index.min.js"></script>
 	<?php wp_footer(); ?>
 </body>
 </html>
